5fcac0ebb712742007b4f1002d8e4493498f3e8c
Michael B <michael@ramyun.co.uk>
2019-01-28 20:02:01 +0000

    Implemented json file parsing

---
+++ FoodFight/Assets/Scripts/RecipeData/RecipeDefinitions.cs
@@ -0,0 +1,16 @@
+﻿[System.Serializable]
+public class RecipeDefinitions {
+	public RecipeDefinition[] recipes;
+}
+
+[System.Serializable]
+public class IngredientCriteria {
+	public string name;
+	public bool cooked, chopped;
+}
+
+[System.Serializable]
+public class RecipeDefinition {
+	public string name;
+	public IngredientCriteria[] ingredients;
+}
