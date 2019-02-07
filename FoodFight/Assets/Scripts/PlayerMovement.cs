bef29e9934e877c5b7fad7b712e95f72bfd474af
electon100 <cf16670@my.bristol.ac.uk>
2019-02-07 12:04:31 +0000

    Player movement for multiple players working

--- FoodFight/Assets/Scripts/PlayerMovement.cs
+++ FoodFight/Assets/Scripts/PlayerMovement.cs
@@ -4,26 +4,39 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour{
 
-    static Vector3 targetPosition;
-    static GameObject movingPlayer;
-    float speed = 5.0f;
+    static Dictionary<GameObject, Vector3> movingPlayers;
+
+    float speed = 10.0f;
+
+    private void Start()
+    {
+        movingPlayers = new Dictionary<GameObject, Vector3>();
+    }
 
     private void Update()
     {
-        if (movingPlayer) {
-            float step = speed * Time.deltaTime; // calculate distance to move
-            movingPlayer.transform.position = Vector3.MoveTowards(movingPlayer.transform.position, targetPosition, step);
-
-            if (Vector3.Distance(movingPlayer.transform.position, targetPosition) < 0.001f)
+        if (movingPlayers.Count > 0) {
+            List<GameObject> toBeDestroyed = new List<GameObject>();
+            foreach (KeyValuePair<GameObject, Vector3> movingPlayer in movingPlayers)
             {
-                movingPlayer.transform.position = targetPosition;
+                float step = speed * Time.deltaTime; // calculate distance to move
+                movingPlayer.Key.transform.position = Vector3.MoveTowards(movingPlayer.Key.transform.position, movingPlayer.Value, step);
+
+                if (Vector3.Distance(movingPlayer.Key.transform.position, movingPlayer.Value) < 0.001f)
+                {
+                    movingPlayer.Key.transform.position = movingPlayer.Value;
+                    toBeDestroyed.Add(movingPlayer.Key);
+                }
+            }
+            foreach (GameObject player in toBeDestroyed)
+            {
+                movingPlayers.Remove(player);
             }
         }
     }
 
     static public void movePlayer(Vector3 stationPosition, GameObject player)
     {
-        movingPlayer = player;
-        targetPosition = stationPosition;
+        movingPlayers.Add(player, stationPosition);
     }
 }
