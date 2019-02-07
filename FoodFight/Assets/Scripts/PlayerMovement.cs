61ab41cd8da717095de043e452d6578b657eafae
electon100 <cf16670@my.bristol.ac.uk>
2019-02-07 10:50:16 +0000

    Players moving slowly to stations separate of server

--- FoodFight/Assets/Scripts/PlayerMovement.cs
+++ FoodFight/Assets/Scripts/PlayerMovement.cs
@@ -4,24 +4,26 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour{
 
-    static Transform targetPosition;
+    static Vector3 targetPosition;
     static GameObject movingPlayer;
-    float speed = 1.0f;
+    float speed = 5.0f;
 
     private void Update()
     {
-        float step = speed * Time.deltaTime; // calculate distance to move
-        movingPlayer.transform.position = Vector3.MoveTowards(movingPlayer.transform.position, targetPosition.position, step);
+        if (movingPlayer) {
+            float step = speed * Time.deltaTime; // calculate distance to move
+            movingPlayer.transform.position = Vector3.MoveTowards(movingPlayer.transform.position, targetPosition, step);
 
-        if (Vector3.Distance(movingPlayer.transform.position, targetPosition.position) < 0.001f)
-        {
-            movingPlayer.transform.position = targetPosition.position;
+            if (Vector3.Distance(movingPlayer.transform.position, targetPosition) < 0.001f)
+            {
+                movingPlayer.transform.position = targetPosition;
+            }
         }
     }
 
     static public void movePlayer(Vector3 stationPosition, GameObject player)
     {
         movingPlayer = player;
-        targetPosition.position = stationPosition;
+        targetPosition = stationPosition;
     }
 }
