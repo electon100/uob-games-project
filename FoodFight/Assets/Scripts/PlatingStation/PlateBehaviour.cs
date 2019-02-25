e5188e168683ecacaf0f763b21cc33a2ad0f5d17
Michael B <michael@ramyun.co.uk>
2019-02-25 11:50:15 +0000

    Modified plating consistency

--- FoodFight/Assets/Scripts/PlatingStation/PlateBehaviour.cs
+++ FoodFight/Assets/Scripts/PlatingStation/PlateBehaviour.cs
@@ -1,44 +1,87 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
-using System.Runtime.Serialization.Formatters.Binary;
-using System.IO;
+using UnityEngine.UI;
 using System.Text;
 
 public class PlateBehaviour : MonoBehaviour {
 
+    private readonly string stationID = "3";
+
     public Player player;
-    // All ingredients on the plate
-    public List<Ingredient> ingredientList = new List<Ingredient>();
-    public List<GameObject> ingredientObjects = new List<GameObject>();
-    // Text representation of ingredients on Screen
+
+    /* Text representation of ingredients on Screen */
     public Text ingredientListText;
-    // Holds the name of the recipe
-    Ingredient recipe = null;
 
-    //Final model to display on Plate
-    GameObject model;
+    /* Ingredient stuff */
+    private List<Ingredient> plateContents = new List<Ingredient>();
+  	private List<GameObject> plateContentsObjects = new List<GameObject>();
 
-    // Use this for initialization
     void Start () {
-        DontDestroyOnLoad(GameObject.Find("Player"));
-        ingredientListText = GameObject.Find("Ingredient List").GetComponent<Text>();
-        player = GameObject.Find("Player").GetComponent<Player>();
-        ingredientList = Player.ingredientsFromStation;
-        displayFood();
+
+      Screen.orientation = ScreenOrientation.Portrait;
+
+
+      player = GameObject.Find("Player").GetComponent<Player>();
+
+      clearPlate();
+
+      foreach (Ingredient ingredient in Player.ingredientsFromStation) {
+        addIngredientToPan(ingredient);
+      }
+
+      // displayFood();
 	  }
 
     void Update() {
-      ingredientList = Player.ingredientsFromStation;
+      // ingredientList = Player.ingredientsFromStation;
+      updateTextList();
+    }
+
+    private void addIngredientToPlate(Ingredient ingredient) {
+      GameObject ingred = (GameObject) Resources.Load(ingredient.Model, typeof(GameObject));
+      Transform ingredTransform = ingred.GetComponentsInChildren<Transform>(true)[0];
+      Quaternion ingredRotation = ingredTransform.rotation;
+      Vector3 ingredPosition = ingredTransform.position + new Vector3(Random.Range(-1, 1), Random.Range(-1, 1), 0);
+      GameObject inst = Instantiate(ingred, ingredPosition, ingredRotation);
+
+      plateContents.Add(ingredient);
+      plateContentsObjects.Add(inst);
+	  }
+
+    private void clearStation() {
+      clearPlate();
+      player.clearIngredientsInStation(stationID);
+    }
+
+    private void clearPlate() {
+      foreach (GameObject go in plateContentsObjects) Destroy(go);
+
+      plateContents.Clear();
+      plateContentsObjects.Clear();
+    }
+
+    public void placeHeldIngredientInPlate() {
+      /* Add ingredient */
+      if (Player.currentIngred != null) {
+        addIngredientToPlate(Player.currentIngred);
+
+        /* Notify server that player has placed ingredient */
+        player.notifyServerAboutIngredientPlaced(Player.currentIngred);
+
+        player.removeCurrentIngredient();
+
+        /* Try combine! */
+      } else {
+        /* TODO: What happens when player is not holding an ingredient */
+      }
     }
 
     void updateTextList() {
       ingredientListText.text = "Current Ingredients:\n";
 
-      foreach(Ingredient ingredient in ingredientList) {
+      foreach(Ingredient ingredient in plateContents) {
         ingredientListText.text += ingredient.ToString() + "\n";
       }
     }
@@ -48,31 +91,6 @@ public class PlateBehaviour : MonoBehaviour {
       Debug.Log(recipe.Name);
     }
 
-    void displayFood() {
-      checkRecipe();
-      Destroy(model, 0.0f);
-      if (ingredientList.Count > 0) {
-        GameObject food = (GameObject) Resources.Load(recipe.Model, typeof(GameObject));
-        Transform modelTransform = food.GetComponentsInChildren<Transform>(true)[0];
-        Quaternion modelRotation = modelTransform.rotation;
-        clearPlateObjects();
-        if (food == null) {
-            //food = (GameObject) Resources.Load("mushPlatePrefab", typeof(GameObject));
-            if (ingredientList.Count > ingredientObjects.Count)
-            {
-                foreach (Ingredient ingred in ingredientList)
-                {
-                    addIngredientToPlate(ingred);
-                }
-            }
-        }
-        model = (GameObject)Instantiate(food, modelTransform.position, modelRotation);
-      } else {
-        model = null;
-      }
-      updateTextList();
-    }
-
     public void serveFood() {
       if (!string.Equals(recipe.Name, "mush")) {
         player.sendScoreToServer(recipe);
@@ -80,46 +98,9 @@ public class PlateBehaviour : MonoBehaviour {
       }
     }
 
-    public void addIngredient() {
-      if (Player.currentIngred != null) {
-        addIngredientToPlate(Player.currentIngred);
-        player.notifyServerAboutIngredientPlaced(Player.currentIngred);
-        player.removeCurrentIngredient();
-        ingredientList = Player.ingredientsFromStation;
-        displayFood();
-      }
-    }
-
-    private void addIngredientToPlate(Ingredient ingredient)
-    {
-        GameObject ingred = (GameObject)Resources.Load(ingredient.Model, typeof(GameObject));
-        Transform ingredTransform = ingred.GetComponentsInChildren<Transform>(true)[0];
-        Quaternion ingredRotation = ingredTransform.rotation;
-        Vector3 ingredPosition = ingredTransform.position + new Vector3(Random.Range(-1, 1), Random.Range(-1, 1), 0);
-        GameObject inst = Instantiate(ingred, ingredPosition, ingredRotation);
-        ingredientObjects.Add(inst);
-    }
-
     public void goBack() {
       /* Notify server that player has left the station */
-      player = GameObject.Find("Player").GetComponent<Player>();
-      player.notifyAboutStationLeft("3");
+      player.notifyAboutStationLeft(stationID);
       SceneManager.LoadScene("PlayerMainScreen");
-    }
-
-    public void clearPlate()
-    {
-        ingredientList.Clear();
-        player.clearIngredientsInStation("3");
-        recipe = null;
-        clearPlateObjects();
-    }
-
-    private void clearPlateObjects()
-    {
-        foreach(GameObject ingred in ingredientObjects) Destroy(ingred, 0.0f);
-
-        ingredientObjects.Clear();
-        Destroy(model, 0.0f);
     }
 }
