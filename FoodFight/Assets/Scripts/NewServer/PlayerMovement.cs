4791112b11d02df451de04b28a925ec1044dd740
Benji <Benji@IT060172.users.bris.ac.uk>
2019-03-26 15:02:49 +0000

    Fixed player moving to centre at start of game

--- FoodFight/Assets/Scripts/NewServer/PlayerMovement.cs
+++ FoodFight/Assets/Scripts/NewServer/PlayerMovement.cs
@@ -5,19 +5,28 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour{
     float speed = 10.0f;
 	Vector3 targetPosition;
+	bool needsMoving;
+
+	private void Start(){
+		needsMoving = false;
+	}
 
     private void Update()
     {
-		float step = speed * Time.deltaTime; // calculate distance to move
-		this.transform.position = Vector3.MoveTowards(this.transform.position, targetPosition, step);
+		if (needsMoving){
+			float step = speed * Time.deltaTime; // calculate distance to move
+			this.transform.position = Vector3.MoveTowards(this.transform.position, targetPosition, step);
 
-		if (Vector3.Distance(this.transform.position, targetPosition) < 0.001f){
-			this.transform.position = targetPosition;
+			if (Vector3.Distance(this.transform.position, targetPosition) < 0.001f){
+				this.transform.position = targetPosition;
+				needsMoving = false;
+			}
 		}
     }
 
     public void movePlayer(Vector3 newPosition)
     {
 		targetPosition = newPosition;
+		needsMoving = true;
     }
 }
